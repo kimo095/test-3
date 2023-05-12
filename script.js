69ef48ba7b9ba1e2c704f791b3e03986014f2e7a
kimo095 <eng095karim@gmail.com>
2023-05-12 12:29:16 +0200

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('contact-form');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent the form from submitting normally
+  event.preventDefault(); 
 
   const formData = new FormData(form);
   const name = formData.get('name');
