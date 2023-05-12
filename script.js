9604ea2f8b79e19d7eec10c77d3af871213352b9
kimo095 <eng095karim@gmail.com>
2023-05-12 12:26:00 +0200

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const form = document.querySelector('form');
-const submitBtn = form.querySelector('button');
+const form = document.getElementById('contact-form');
 
-submitBtn.addEventListener('click', function(event) {
-  event.preventDefault(); 
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting normally
+
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const message = formData.get('message');
+
+  console.log(`Name: ${name}`);
+  console.log(`Email: ${email}`);
+  console.log(`Message: ${message}`);
 });
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
-
-const name = nameInput.value;
-const email = emailInput.value;
-const message = messageInput.value;
-
-
